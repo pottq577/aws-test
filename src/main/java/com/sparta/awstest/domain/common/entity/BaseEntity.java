d3b057195848f327ab0867c92869393339eeb7e6
hyun2y00 <pottq577@naver.com>
2025-06-14 15:49:28 +0900

    feat: 유저 도메인 엔티티 설계

---
+++ src/main/java/com/sparta/awstest/domain/common/entity/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.sparta.awstest.domain.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class  BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/sparta/awstest/domain/user/entity/User.java
@@ -0,0 +1,24 @@
+package com.sparta.awstest.domain.user.entity;
+
+import com.sparta.awstest.domain.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+}

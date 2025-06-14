91f859e4eed459963d0e00c8ad0191b82a910dc9
hyun2y00 <pottq577@naver.com>
2025-06-14 15:56:20 +0900

    feat: BaseEntity에 생성/수정일 추가

--- src/main/java/com/sparta/awstest/domain/common/entity/BaseEntity.java
+++ src/main/java/com/sparta/awstest/domain/common/entity/BaseEntity.java
@@ -5,7 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -16,4 +19,10 @@ public abstract class  BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 }

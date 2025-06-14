343beec3b63d935a62fb6ff8a3016a670ff3d0d8
hyun2y00 <pottq577@naver.com>
2025-06-14 15:49:17 +0900

    feat: Jpa 설정 클래스 추가

---
+++ src/main/java/com/sparta/awstest/global/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.sparta.awstest.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}

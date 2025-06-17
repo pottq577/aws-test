1497bf346b9437d69777bb4ae609a6f32609c6c0
hyun2y00 <pottq577@naver.com>
2025-06-17 16:02:17 +0900

    feat: add table annotation at user entity

--- src/main/java/com/sparta/awstest/domain/user/entity/User.java
+++ src/main/java/com/sparta/awstest/domain/user/entity/User.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(name = "users")
 @Entity
 @Getter
 @NoArgsConstructor

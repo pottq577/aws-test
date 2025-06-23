b1ce98421b260503a3638b116b1bb550c6a14638
hyun2y00 <pottq577@naver.com>
2025-06-23 14:46:08 +0900

    chore: login and push to docker hub

--- src/main/java/com/sparta/awstest/AwsTestApplication.java
+++ src/main/java/com/sparta/awstest/AwsTestApplication.java
@@ -2,12 +2,15 @@ package com.sparta.awstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AwsTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AwsTestApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(AwsTestApplication.class, args);
+        String dbUrl = ctx.getEnvironment().getProperty("spring.datasource.url");
+        System.out.println("dbUrl = " + dbUrl);
     }
 
 }
